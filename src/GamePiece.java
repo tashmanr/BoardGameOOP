73ce64436e3e4142107d1b06be763f4654a792bf
tashmanr <rebeccatashman@gmail.com>
2022-02-08 11:45:05 +0200

    begin game logic

--- src/GamePiece.java
+++ src/GamePiece.java
@@ -1,2 +1,7 @@
-public class GamePiece {
+import java.util.List;
+
+public abstract class GamePiece {
+    private Tuple<Integer, Integer> location;
+
+    public abstract List<Tuple<Integer, Integer>> getMoves();
 }
