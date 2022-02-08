fd4cf42c395692a7e040c4fcc93bf6c3a99ef2b1
LolaSi <72922569+LolaSi@users.noreply.github.com>
2022-02-08 17:13:31 +0200

    Update HumanPlayer.java
    Lola - added do-while for user input in makeMove

--- src/HumanPlayer.java
+++ src/HumanPlayer.java
@@ -14,11 +14,26 @@ public class HumanPlayer extends Player{
     }
     
     public void makeMove(Game game){
-        System.out.print("please enter your desired move:\n");
-        String str= sc.nextLine();
-        String[] arrOfStr = str.split(", ", 4);
+        
+        Boolean valid_input = false;
+        do{
+            System.out.print("please enter your desired move:\n");
+            String str= sc.nextLine();
+            String[] arrOfStr = str.split("[ ,]+");
 
-        // TODO check the split works correctly
+            System.out.print(str);
+            System.out.print("\n\n");
+            System.out.print(arrOfStr.length);
+
+            if(arrOfStr.length != 4){
+                System.out.print("Incorrect Input! Try again! \n");
+            }
+            else{
+                valid_input = true;
+            }
+        }while(!valid_input);
+
+
         Tuple<Integer, Integer> start = new Tuple(Integer.parseInt(arrOfStr[0]), Integer.parseInt(arrOfStr[1]));
         Tuple<Integer, Integer> end = new Tuple(Integer.parseInt(arrOfStr[3]), Integer.parseInt(arrOfStr[4]));
 
