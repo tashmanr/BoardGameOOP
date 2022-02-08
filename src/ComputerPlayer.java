7434b0415ceafff78dcbab31ad4c8c7d925a4f03
LolaSi <72922569+LolaSi@users.noreply.github.com>
2022-02-08 13:18:15 +0200

    Create ComputerPlayer.java

---
+++ src/ComputerPlayer.java
@@ -0,0 +1,9 @@
+public class ComputerPlayer extends Player{
+    /**
+     * Updates the game state to take a move for the current player.
+     * @param game	the game to update.
+     */
+    public void makeMove(Game game){
+        return;
+    }
+}
