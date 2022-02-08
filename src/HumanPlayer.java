bf1a79d77d6793222607300899bd0d8f3942955c
LolaSi <72922569+LolaSi@users.noreply.github.com>
2022-02-08 13:17:20 +0200

    Create HumanPlayer.java

---
+++ src/HumanPlayer.java
@@ -0,0 +1,9 @@
+public class HumanPlayer extends Player{
+    /**
+     * Updates the game state to take a move for the current player.
+     * @param game	the game to update.
+     */
+    public void makeMove(Game game){
+        return;
+    }
+}
