3fe085f3deb8769a1ad78a2504d30fc41dbf0ecc
tashmanr <rebeccatashman@gmail.com>
2022-03-02 13:34:18 +0200

    logic for kingMe function

--- src/model/CheckersGame.java
+++ src/model/CheckersGame.java
@@ -5,6 +5,7 @@ import model.boards.CheckersBoard;
 import model.pieces.CheckerPieceOptions;
 import model.pieces.GamePiece;
 import model.players.Player;
+
 import java.lang.Math;
 
 import java.util.ArrayList;
@@ -21,27 +22,31 @@ public class CheckersGame extends Game {
      * @param end a tuple of the move (start, end)
      * @return CheckersPieceOptions enum represents the direction of the move
      */
-    private CheckerPieceOptions getMoveDirection(Tuple<Integer,Integer> start, Tuple<Integer,Integer> end){
-        if (start.x > end.x){
+    private CheckerPieceOptions getMoveDirection(Tuple<Integer, Integer> start, Tuple<Integer, Integer> end) {
+        if (start.x > end.x) {
             return CheckerPieceOptions.Up;
-        }
-        else if (start.x < end.x){
+        } else if (start.x < end.x) {
             return CheckerPieceOptions.Down;
-        }
-        else{
+        } else {
             // this move is not legal
             return CheckerPieceOptions.Invalid;
         }
     }
 
     @Override
-    public Boolean makeMove(ArrayList<Tuple<Integer,Integer>> moves, Player player){
+    public Boolean makeMove(ArrayList<Tuple<Integer, Integer>> moves, Player player) {
         boolean legal = checkMoveLegal(moves, player);
-        if(legal){
-            for(int i=0; i< moves.size()-1; i++){
-                legal = board.makeMove(moves.get(i), moves.get(i+1));
-                if(!legal){
+        if (legal) {
+            for (int i = 0; i < moves.size() - 1; i++) {
+                legal = board.makeMove(moves.get(i), moves.get(i + 1));
+                if (!legal) {
                     return false;
+                } else {
+                    Tuple<Integer, Integer> destination = moves.get(i + 1);
+                    Tuple<Integer, Integer> start = moves.get(i);
+                    if ((destination.x == 0 || destination.x == 7) && board.getBoard()[destination.x][destination.y].toString().startsWith("M")) {
+                        kingMe(destination.x, destination.y);
+                    }
                 }
             }
             return true;
@@ -50,31 +55,31 @@ public class CheckersGame extends Game {
     }
 
     @Override
-    public Boolean checkMoveLegal(ArrayList<Tuple<Integer,Integer>> moves, Player player){
+    public Boolean checkMoveLegal(ArrayList<Tuple<Integer, Integer>> moves, Player player) {
 
         int move_index = 0; // the index of the move in arraylist moves
-        Tuple<Integer,Integer> start = moves.get(move_index);
+        Tuple<Integer, Integer> start = moves.get(move_index);
         move_index++;
 
         // get the piece in start position
         GamePiece movingPiece = board.getPieceByLocation(start);
-        if (movingPiece == null){
+        if (movingPiece == null) {
             return false;
         }
 
         // get moving piece possible direction of movement
         ArrayList<Integer> directions = movingPiece.getMoveDirection();
 
-        do{
-            Tuple<Integer,Integer> next = moves.get(move_index);
+        do {
+            Tuple<Integer, Integer> next = moves.get(move_index);
 
             // check this move is legitimate on the board
-            if(!board.isLegalMove(start, next)){
+            if (!board.isLegalMove(start, next)) {
                 return false;
             }
 
             // check if next position is empty
-            if ( board.getPieceByLocation(next) != null ){
+            if (board.getPieceByLocation(next) != null) {
                 return false;
             }
 
@@ -82,7 +87,7 @@ public class CheckersGame extends Game {
             CheckerPieceOptions moveDirection = getMoveDirection(start, next);
 
             // check if this moveDirection is valid for the gamePiece
-            if (directions.contains(moveDirection.toInt())){
+            if (directions.contains(moveDirection.toInt())) {
                 int jumpNumber = Math.abs(start.x - next.x);
 
                 if (jumpNumber != 1) {
@@ -98,8 +103,8 @@ public class CheckersGame extends Game {
                 move_index++;
             }
             //TODO change the start and next position
-            start = moves.get(move_index-1);
-        }while(move_index < moves.size());
+            start = moves.get(move_index - 1);
+        } while (move_index < moves.size());
 
         return true;
     }
