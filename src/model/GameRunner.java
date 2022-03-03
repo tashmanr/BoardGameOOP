c6fa6f452afe22d4fbf39f2755da29087d61e17a
tashmanr <rebeccatashman@gmail.com>
2022-03-03 13:01:54 +0200

    created runGame() function for GameRunner

--- src/model/GameRunner.java
+++ src/model/GameRunner.java
@@ -9,24 +9,29 @@ public class GameRunner {
     private ScoreBoard scoreBoard;
     private Game game;
     private GameInitializer gameInitializer;
+    private DefaultIO dio;
 
-    private GameRunner(Tuple<Player, Player> players, GameOptions o, boolean newGame, DefaultIO dio) {
+    private GameRunner(DefaultIO dio) {
+        this.dio = dio;
+        scoreBoard = ScoreBoard.getInstance();
+    }
+
+    public static GameRunner getInstance(DefaultIO dio) {
+        if (runnerInstance == null) {
+            runnerInstance = new GameRunner(dio);
+        }
+        return runnerInstance;
+    }
+
+    public void runGame(Tuple<Player, Player> players, GameOptions o, boolean newGame){
         gameInitializer = new GameInitializer();
         game = gameInitializer.getGame(o, players, newGame, dio);
-        scoreBoard = ScoreBoard.getInstance();
         game.start();
         Player winner = game.getWinner();
         if (winner != null) {
             dio.write(winner.getPlayerName() + " is the winner!\n");
             scoreBoard.addGameResults(winner.getPlayerName());
         }
-    }
-
-    public static GameRunner getInstance(Tuple<Player, Player> players, GameOptions o, boolean newGame, DefaultIO dio) {
-        if (runnerInstance == null) {
-            runnerInstance = new GameRunner(players, o, newGame, dio);
-        }
-        return runnerInstance;
     }
 
 
